bca73ee43fd1b603400da8387cb7836e668c649f
vutrungtam619 <vutrungtam619@gmail.com>
2025-08-27 00:17:36 +0700

    add

--- config/config.py
+++ config/config.py
@@ -38,9 +38,9 @@ config = {
         'batch_size': 8,
         'num_workers': 4,
         'init_lr': 0.00033,
-        'epoch': 10,
+        'epoch': 80,
         'ckpt_freq': 2,
-        'log_freq': 10,
+        'log_freq': 25,
         'log_dir': os.path.join(project_root, 'log'),
     }    
 }
